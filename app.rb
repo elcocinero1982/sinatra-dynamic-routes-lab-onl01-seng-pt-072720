6a5621c6ddace6dfd838612d8a7b272ddf4350a1
jose luis <cortezluisjose@gmail.com>
2020-10-05 00:51:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
  get '/reversename/:name' do 
-   
+    @reverse_name = params[:name].reverse
+    @reverse_name
    
  end
   get '/square/:number' do 
