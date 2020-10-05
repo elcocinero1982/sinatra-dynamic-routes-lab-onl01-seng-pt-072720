2a83c06fc8915f08daa2be9f1608d75fc5c8a5d0
jose luis <cortezluisjose@gmail.com>
2020-10-05 00:52:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,11 @@ class App < Sinatra::Base
   end
   
    get '/say/:number/:phrase' do 
-     
+     str = ""
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @num.times { str += "#{@phrase}\n" }
+    str
    end
    
     get '/say/:word1/:word2/:word3/:word4/:word5' do 
